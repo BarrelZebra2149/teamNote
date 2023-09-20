2d3a3ad912f5339958543579a1fa6f6a8f9dc2aa
BarrelZebra2149 <98868703+BarrelZebra2149@users.noreply.github.com>
2023-09-20 16:52:56 +0900

    Create codeforcesTamplate.cpp

---
+++ codeforcesTamplate.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+ll testCnt, x, y, n;
+ll i, j;
+void fastIO() {
+    cin.tie(NULL); ios::sync_with_stdio(false);
+}
+
+void input() {
+    
+}
+
+void solve() {
+    
+}
+
+int main() {
+    fastIO();
+    cin>>testCnt;
+    while(testCnt--) {
+      input();
+      solve();
+    }
+    return 0;
+}
