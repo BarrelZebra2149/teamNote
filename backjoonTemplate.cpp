ee833c1d32ccd494d8b75118fd9d0340450a1b96
BarrelZebra2149 <98868703+BarrelZebra2149@users.noreply.github.com>
2023-09-20 16:52:03 +0900

    Create backjoonTemplate.cpp

---
+++ backjoonTemplate.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define MAX 100000
+#define INF 987654321
+using namespace std;
+using ll = long long;
+
+int i, j;
+double a, b;
+void fastIO() {
+    ios::sync_with_stdio(false); cin.tie(0);
+}
+
+void input() {
+  
+}
+
+void solve() {
+    
+}
+
+int main() {
+    fastIO();
+    input();
+    solve();    
+    return 0;
+}
+
